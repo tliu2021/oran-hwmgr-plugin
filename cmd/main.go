a37b67e019eeae57bda8bde8354be1784eee6679
Allain Legacy <alegacy@redhat.com>
2025-03-04 08:56:58 -0500

    CNF-16877: Replace kube-rbac-proxy for metrics
    This replaces the use of kube-rbac-proxy for protecting the metrics API
    endpoint.  The gcr.io kube-rbac-proxy image has been deprecated and the
    next version of kubebuilder won't rely on it for protecting the
    endpoint, so we are moving ahead with its elimination.
    
    Signed-off-by: Allain Legacy <alegacy@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,8 @@ import (
 	"log/slog"
 	"os"
 
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
+
 	"github.com/openshift-kni/oran-hwmgr-plugin/internal/server"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
@@ -67,19 +69,18 @@ func init() {
 
 func _main() int {
 	var metricsAddr string
+	var tlsCertDir string
 	var enableLeaderElection bool
 	var probeAddr string
-	var secureMetrics bool
 	var enableHTTP2 bool
 	var apiServerAddr string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&tlsCertDir, "tls-cert-dir", "", "The path to the directory containing the TLS certificate and private key.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.StringVar(&apiServerAddr, "api-bind-address", ":8082", "The address the API server binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	flag.BoolVar(&secureMetrics, "metrics-secure", false,
-		"If set the metrics endpoint is served securely")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 	opts := zap.Options{
@@ -131,9 +132,11 @@ func _main() int {
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme: scheme,
 		Metrics: metricsserver.Options{
-			BindAddress:   metricsAddr,
-			SecureServing: secureMetrics,
-			TLSOpts:       tlsOpts,
+			BindAddress:    metricsAddr,
+			SecureServing:  tlsCertDir != "",
+			TLSOpts:        tlsOpts,
+			CertDir:        tlsCertDir,
+			FilterProvider: filters.WithAuthenticationAndAuthorization,
 		},
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
